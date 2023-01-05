414b70f212857bdcfec340afcc072313be4df6e2
Eziamaka Promise <107619759+eziamakapromise@users.noreply.github.com>
2023-01-05 03:47:15 -0800

    Add files via upload

---
+++ pages/contact.vue
@@ -0,0 +1,51 @@
+<script>
+import feather from "feather-icons";
+
+export default {
+  scrollToTop: true,
+  data: () => {
+    return {
+      contacts: [
+        {
+          id: 1,
+          name: "Nigeria",
+          icon: "map-pin",
+        },
+        {
+          id: 2,
+          name: "tobeeziamaaka999@gmail.com",
+          icon: "mail",
+        },
+        {
+          id: 3,
+          name: "+2348026079237",
+          icon: "phone",
+        },
+      ],
+    };
+  },
+  mounted() {
+    feather.replace();
+  },
+  updated() {
+    feather.replace();
+  },
+};
+</script>
+
+<template>
+  <div
+    class="
+      container
+      mx-auto
+      flex flex-col-reverse
+      md:flex-row md:py-10 md:mt-20
+    "
+  >
+    <!-- Contact form -->
+    <ContactForm />
+
+    <!-- Contact details -->
+    <ContactDetails :contacts="contacts" />
+  </div>
+</template>
---
+++ store/index.js
@@ -0,0 +1,597 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const state = () => ({
+  projectsHeading: "Projects Portfolio",
+  projectsDescription: "Some of the projects I have successfully completed",
+  projects: [
+    {
+      id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccdb",
+      title: "TIC-TAC-TOE GAME",
+      category: "Web Application",
+      img: "../images/mobile-project-2.png",
+      publishDate: "2022",
+      tag: "Frontend",
+      clientTitle: "Info",
+      objectivesTitle: "",
+      objectivesDetails:
+        "",
+      techTitle: "Tools & Technologies",
+      detailsTitle: "",
+      socialTitle: "Share This",
+      projectImages: [
+        {
+          id: uuidv4(),
+          title: "TIC-TAC-TOE GAME",
+          img: "../images/mobile-project-2.png",
+        },
+        {
+          id: uuidv4(),
+          title: "TIC-TAC-TOE GAME",
+          img: "../images/mobile-project-2.png",
+        },
+        {
+          id: uuidv4(),
+          title: "TIC-TAC-TOE GAME",
+          img: "../images/mobile-project-2.png",
+        },
+      ],
+      companyInfos: [
+        {
+          id: uuidv4(),
+          title: "Hosting",
+          details: "Netlify",
+        },
+        {
+          id: uuidv4(),
+          title: "Demo",
+          details: "https://xoxo-game-vuejs.netlify.app",
+        },
+        {
+          id: uuidv4(),
+          title: "GitHub Repo",
+          details: "https://github.com/eziamakapromise/vuejs-game-xoxo",
+        },
+
+      ],
+      technologies: [
+        "HTML",
+        "CSS",
+        "Vue.js",
+        "Nuxt.js",
+        "TailwindCSS",
+      ],
+
+      socialSharings: [
+        {
+          id: uuidv4(),
+          name: "Twitter",
+          icon: "twitter",
+          url: "https://twitter.com",
+        },
+        {
+          id: uuidv4(),
+          name: "Instagram",
+          icon: "instagram",
+          url: "https://instagram.com",
+        },
+        {
+          id: uuidv4(),
+          name: "Facebook",
+          icon: "facebook",
+          url: "https://facebook.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "LinkedIn",
+          icon: "linkedin",
+          url: "https://linkedin.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Youtube",
+          icon: "youtube",
+          url: "https://www.youtube.com/",
+        },
+      ],
+    },
+    {
+      id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccdc",
+      title: "SIMPLE CALCULATOR",
+      category: "Web Application",
+      img: "../images/web-project-1.jpg",
+      publishDate: "2022",
+      tag: "Frontend",
+      clientTitle: "Info",
+      objectivesTitle: "",
+      objectivesDetails:
+        "",
+      techTitle: "Tools & Technologies",
+      detailsTitle: "",
+      socialTitle: "Share This",
+      projectImages: [
+        {
+          id: uuidv4(),
+          title: "Simple Web calculator",
+          img: "../images/web-project-1.jpg",
+        },
+        {
+          id: uuidv4(),
+          title: "Simple Web calculator",
+          img: "../images/web-project-2.jpg",
+        },
+        {
+          id: uuidv4(),
+          title: "Simple Web calculator",
+          img: "../images/web-project-2.jpg",
+        },
+      ],
+      companyInfos: [
+        {
+          id: uuidv4(),
+          title: "Hosting",
+          details: "Netlify",
+        },
+        {
+          id: uuidv4(),
+          title: "Demo",
+          details: "https://calculator-with-vuejs.netlify.app",
+        },
+        {
+          id: uuidv4(),
+          title: "GitHub Repo",
+          details: "https://github.com/eziamakapromise/vuejs-calculator",
+        },
+
+      ],
+      technologies: [
+        "HTML",
+        "CSS",
+        "Vue.js",
+        "Nuxt.js",
+        "TailwindCSS",
+      ],
+
+
+      socialSharings: [
+        {
+          id: uuidv4(),
+          name: "Twitter",
+          icon: "twitter",
+          url: "https://twitter.com",
+        },
+        {
+          id: uuidv4(),
+          name: "Instagram",
+          icon: "instagram",
+          url: "https://instagram.com",
+        },
+        {
+          id: uuidv4(),
+          name: "Facebook",
+          icon: "facebook",
+          url: "https://facebook.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "LinkedIn",
+          icon: "linkedin",
+          url: "https://linkedin.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Youtube",
+          icon: "youtube",
+          url: "https://www.youtube.com",
+        },
+      ],
+    },
+    {
+      id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccdy",
+      title: "Movies Review",
+      category: "Web Application",
+      img: "../images/mobile-project-1.png",
+      publishDate: "2022",
+      tag: "Frontend",
+      clientTitle: "Info",
+      objectivesTitle: "",
+      objectivesDetails:
+        "",
+      techTitle: "Tools & Technologies",
+      detailsTitle: "",
+      socialTitle: "Share This",
+      projectImages: [
+        {
+          id: uuidv4(),
+          title: "Movies Review",
+          img: "../images/mobile-project-1.png",
+        },
+        {
+          id: uuidv4(),
+          title: "Movies Review",
+          img: "../images/mobile-project-1.png",
+        },
+        {
+          id: uuidv4(),
+          title: "Movies Review",
+          img: "../images/mobile-project-1.png",
+        },
+      ],
+      companyInfos: [
+        {
+          id: uuidv4(),
+          title: "Hosting",
+          details: "Netlify",
+        },
+        {
+          id: uuidv4(),
+          title: "Demo",
+          details: "https://moviesreview20.netlify.app",
+        },
+        {
+          id: uuidv4(),
+          title: "Website",
+          details: "https://github.com/eziamakapromise/vuejs-movie-review",
+        },
+
+      ],
+      technologies: [
+        "HTML",
+        "CSS",
+        "API",
+        "Vue.js",
+      ],
+
+      socialSharings: [
+        {
+          id: uuidv4(),
+          name: "Twitter",
+          icon: "twitter",
+          url: "https://twitter.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Instagram",
+          icon: "instagram",
+          url: "https://instagram.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Facebook",
+          icon: "facebook",
+          url: "https://facebook.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "LinkedIn",
+          icon: "linkedin",
+          url: "https://linkedin.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Youtube",
+          icon: "youtube",
+          url: "https://www.youtube.com/",
+        },
+      ],
+    },
+    {
+      id: "260d1271-f1f2-4bc9-a5eb-05a89c8ccc99",
+      title: "Portfolio v1",
+      category: "Web Application",
+      img: "../images/web-project-2.png",
+      publishDate: "2022",
+      tag: "Web / Frontend",
+      clientTitle: "Info",
+      objectivesTitle: "",
+      objectivesDetails:
+        "",
+      techTitle: "Tools & Technologies",
+      detailsTitle: "",
+      socialTitle: "Share This",
+      projectImages: [
+        {
+          id: uuidv4(),
+          title: "Portfolio v1",
+          img: "../images/web-project-2.png",
+        },
+        {
+          id: uuidv4(),
+          title: "Portfolio v1",
+          img: "../images/web-project-2.png",
+        },
+        {
+          id: uuidv4(),
+          title: "Portfolio v1",
+          img: "../images/web-project-2.png",
+        },
+      ],
+      companyInfos: [
+        {
+          id: uuidv4(),
+          title: "Hosting",
+          details: "Netlify",
+        },
+        {
+          id: uuidv4(),
+          title: "  Demo",
+          details: "https://eziamakapromise.netlify.app",
+        },
+        {
+          id: uuidv4(),
+          title: "GitHub Repo",
+          details: "https://github.com/eziamakapromise/resume",
+        },
+
+      ],
+      technologies: [
+        "HTML",
+        "CSS",
+        "SASS",
+        "JavaScript",
+      ],
+
+      socialSharings: [
+        {
+          id: uuidv4(),
+          name: "Twitter",
+          icon: "twitter",
+          url: "https://twitter.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Instagram",
+          icon: "instagram",
+          url: "https://instagram.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Facebook",
+          icon: "facebook",
+          url: "https://facebook.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "LinkedIn",
+          icon: "linkedin",
+          url: "https://linkedin.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Youtube",
+          icon: "youtube",
+          url: "https://www.youtube.com/",
+        },
+      ],
+    },
+    {
+      id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccdm",
+      title: "Portfolio v2",
+      category: "Web Application",
+      img: "../images/ui-project-1.png",
+      publishDate: "2022",
+      tag: "Frontend",
+      clientTitle: "Info",
+      objectivesTitle: "",
+      objectivesDetails:
+        "",
+      techTitle: "Tools & Technologies",
+      detailsTitle: "",
+      socialTitle: "Share This",
+      projectImages: [
+        {
+          id: uuidv4(),
+          title: "Portfolio v2",
+          img: "../images/ui-project-1.png",
+        },
+        {
+          id: uuidv4(),
+          title: "Portfolio v2",
+          img: "../images/ui-project-1-2.png",
+        },
+        {
+          id: uuidv4(),
+          title: "Portfolio v2",
+          img: "../images/ui-project-1.png",
+        },
+      ],
+      companyInfos: [
+        {
+          id: uuidv4(),
+          title: "Hosting",
+          details: "Netlify",
+        },
+        {
+          id: uuidv4(),
+          title: "Demo",
+          details: "https://devpromise.netlify.app",
+        },
+        {
+          id: uuidv4(),
+          title: "GitHub Repo",
+          details: "https://github.com/eziamakapromise/nuxtjs-portfolio",
+        },
+
+      ],
+      technologies: [
+        "HTML",
+        "CSS",
+        "Vuex",
+        "Nuxt.js",
+        "TailwindCSS",
+      ],
+      socialSharings: [
+        {
+          id: uuidv4(),
+          name: "Twitter",
+          icon: "twitter",
+          url: "https://twitter.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Instagram",
+          icon: "instagram",
+          url: "https://instagram.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Facebook",
+          icon: "facebook",
+          url: "https://facebook.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "LinkedIn",
+          icon: "linkedin",
+          url: "https://linkedin.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Youtube",
+          icon: "youtube",
+          url: "https://www.youtube.com/",
+        },
+      ],
+    },
+    {
+      id: "260d1271-f1f2-4bc9-a5eb-05a89c8cccbb",
+      title: "AllAboutSoftwareNG",
+      category: "VideoEditing",
+      img: "../images/ui-project-2.png",
+      publishDate: "2022",
+      tag: "Video Editor",
+      clientTitle: "Info",
+      objectivesTitle: "Objective",
+      objectivesDetails:
+        "To let people know on what to do on there tech career,where to go & things they are going to face in the tech career",
+      techTitle: "Tools & Technologies",
+      detailsTitle: "",
+      socialTitle: "Share This",
+      companyInfos: [
+        {
+          id: uuidv4(),
+          title: "Channel",
+          details: "AllAboutSoftwareNG",
+        },
+        {
+          id: uuidv4(),
+          title: "Service",
+          details: "Post contents for Techies",
+        },
+        {
+          id: uuidv4(),
+          title: "Channel Url",
+          details: "https://www.youtube.com/@AllAboutSoftwareNG",
+        },
+        {
+          id: uuidv4(),
+          title: "email",
+          details: "eziamakapromise@gmail.com",
+        },
+      ],
+      technologies: [
+        "CAPCUT",
+        "CANVA",
+        "MICROPHONE",
+        "OBS STUDIO",
+        "VMIX",
+        "YOUTUBE STUDIO",
+      ],
+
+      socialSharings: [
+        {
+          id: uuidv4(),
+          name: "Twitter",
+          icon: "twitter",
+          url: "https://twitter.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Instagram",
+          icon: "instagram",
+          url: "https://instagram.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Facebook",
+          icon: "facebook",
+          url: "https://facebook.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "LinkedIn",
+          icon: "linkedin",
+          url: "https://linkedin.com/",
+        },
+        {
+          id: uuidv4(),
+          name: "Youtube",
+          icon: "youtube",
+          url: "https://www.youtube.com/@AllAboutSoftwareNG",
+        },
+      ],
+    },
+  ],
+  aboutMe: [
+    {
+      id: uuidv4(),
+      bio: "I am Frontend Engineer, i love coding its my hobby,my tech career started when i was 13 years old i started with PYTHON i did not know what i was doing i was taking advise here and there i was in a confused state which made me to give up on my tech career i started video editing i started animations with BLENDER, BLENDER was the technology i  used then before new editing technologies came out ,at the age of 15 while i was scrolling on youtube shorts i came across videos of succesful frontend devs and what they built and i was amazed and would  love to build webapps like that.Then i started the tech journey again but this time as a frontend engineer i strted with HTML,CSS,TAIWIND CSS,JAVASCRIPT then i decide to learn a framework of JAVASCRIPT and i chose REACTJS but REACTJS wasn't too fun for me then i chose VUEJS which was much fun to me then learnt  a framework of VUEJS which is NUXTJS and till date i  work with VUEJS & NUXTJS.",
+    },
+    {
+      id: uuidv4(),
+      bio: "My first job as a frontend engineer was to build a voting platform for  social media influencers i got paid,i also had personnal projects which are listed in my projects page then i was called for another at an e-commerce store Briton & Kay Store since then i have been getting more gigs.I am gratefull to GOOGLE for their  events cause it has change and impacted me i really apprectiate the community.",
+    },
+  ],
+  Date: new Date().getFullYear(),
+  socialProfiles: [
+    {
+      id: uuidv4(),
+      name: "GitHub",
+      icon: "github",
+      url: "https://github.com/eziamakapromise",
+    },
+    {
+      id: uuidv4(),
+      name: "Twitter",
+      icon: "twitter",
+      url: "https://twitter.com/EziamakaPromise",
+    },
+    {
+      id: uuidv4(),
+      name: "Medium",
+      icon: "book",
+      url: "https://medium.com/@tobeeziamaaka999",
+    },
+    {
+      id: uuidv4(),
+      name: "Instagram",
+      icon: "instagram",
+      url: "https://instagram.com/eziamakapromise_9",
+    },
+  ],
+  categories: [
+    {
+      id: uuidv4(),
+      value: "web",
+      name: "Web Application",
+    },
+    {
+      id: uuidv4(),
+      value: "videoediting",
+      name: "Videos Editing",
+    },
+  ],
+});
+
+export const getters = {
+  getProjectById: (state) => (id) => {
+    return state.projects.find((project) => project.id == id);
+  },
+};
+
+export const mutations = {
+  // @todo
+};
+
+export const actions = {
+  // @todo
+};
